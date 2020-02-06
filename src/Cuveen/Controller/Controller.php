438bb4a704266cf08bc862aa1989d205a27f6746
Cuveen <60683289+cuveen@users.noreply.github.com>
2020-02-06 12:29:41 +0700

    Update Controller.php

--- src/Cuveen/Controller/Controller.php
+++ src/Cuveen/Controller/Controller.php
@@ -2,11 +2,36 @@
 
 
 namespace Cuveen\Controller;
-use Cuveen\App;
+use Cuveen\Auth\Auth;
+use Cuveen\Config\Config;
+use Cuveen\Database\DB;
 use Cuveen\Exception\CuveenException;
+use Cuveen\Hash\Security;
+use Cuveen\Http\Request;
+use Cuveen\Router\Router;
+use Cuveen\Session\Session;
+use Cuveen\View\View;
 
-class Controller extends App
+class Controller
 {
+    protected $db;
+    protected $request;
+    protected $session;
+    protected $security;
+    public $config;
+    public $router;
+    protected $auth;
+    protected $view;
+    public function __construct()
+    {
+        $this->db = DB::getInstance();
+        $this->request = Request::getInstance();
+        $this->security = Security::getInstance();
+        $this->config = Config::getInstance();
+        $this->session = Session::getInstance();
+        $this->auth = Auth::getInstance();
+        $this->view = View::getInstance();
+    }
     public function model($model, $attr = false)
     {
         if(file_exists($this->base_path.DIRECTORY_SEPARATOR.'models'.DIRECTORY_SEPARATOR.$model.'.php')){
